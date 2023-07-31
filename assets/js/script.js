321559ac25a74c9426b0f01a37722277c74577e6
Muhamad Rafli <yusfiwawan2@gmail.com>
2023-07-31 14:59:17 +0700

    feat: add footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,7 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menuMobile = document.getElementById('mobileMenu');
 
-
-// Ketika hamburger menu di-klik
 hamburgerMenu.addEventListener('click', function () {
-    // Toggle class 'active' pada elemen hamburger-menu
     this.classList.toggle('active');
 
     menuMobile.classList.toggle('active');
